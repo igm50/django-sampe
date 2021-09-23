2939043204325ec440c66400ffd055991e3a7a53
jintz <keisuke.igimi@gmail.com>
2021-09-23 10:36:40 +0900

    feat: 都市情報をモデル化する

--- src/app/settings.py
+++ src/app/settings.py
@@ -9,6 +9,7 @@ DEBUG = True
 # ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'cities',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/cities/views.py
+++ src/cities/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from .models import City
+
 
 def index(request):
-  return HttpResponse("Hello world!!")
+    sample = City.objects.filter(id=5)[0]
+    return HttpResponse('id: 5'
+                        + ', name: ' + sample.name
+                        + ', country: ' + sample.countrycode.name
+                        + ', district: ' + sample.district
+                        + ', population: ' + str(sample.population))
